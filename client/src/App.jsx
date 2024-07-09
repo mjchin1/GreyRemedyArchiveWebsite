ba747bb3695961b65c6013d346bd0fdeda8815c6
Melissa J. Chin <melissa1jchin@gmail.com>
2024-07-09 15:24:33 -0500

    updating nav bar design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <>
 
       <div className="navBanner">
+      <h2>LandingPage</h2>
       <Navigations/>
       </div>
 
--- client/src/components/Navigations.jsx
+++ client/src/components/Navigations.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 export default function Navigations() {
     return (
         <nav>
+            <Link to = '/'>Home</Link>
             <Link to = '/gallery'>Gallery</Link>
         </nav>
     );
