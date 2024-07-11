dba47b0ac534072c64062f2185a59366b78d12b2
Melissa J. Chin <melissa1jchin@gmail.com>
2024-07-11 14:03:17 -0500

    adding content to About page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,6 +1,19 @@
 
 export default function About() {
   return (
+
+    <>
       <h1>About</h1>
+
+      <div className="about">
+
+      <p> Grey Remedy was a ceramic décor line produced by Melissa Chin from 2017 to 2023. </p>
+      
+      <p>Through Grey Remedy, Melissa explored the creation of art as a meditative practice while producing elegant, minimalist pieces that were carefully crafted with an emphasis on the importance of shape, texture and proportion. Her goal was to create objects that brought beauty to our daily routines, turning the parts of our day that we tend to rush through into calm, intentional moments.</p>
+
+      <p>Through her work, Melissa explored the tension between contrast and harmony, balancing curves and clean lines to produce beautifully unexpected shape combinations. She maintained a simple palette of black, white, and earth tones to challenge herself to innovate with form, experimenting with dimension and configuration to create distinctive pieces that could speak for themselves with little addition of color.</p> 
+
+      </div>
+      </>
   );
 };
