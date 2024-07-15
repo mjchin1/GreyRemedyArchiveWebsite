95d3c7a86d1f4fcb2121724935c490b5aca35462
Melissa J. Chin <melissa1jchin@gmail.com>
2024-07-15 09:13:23 -0500

    added GR logo, updated About text, changed browser heading

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -18,7 +18,7 @@ export default function About() {
       
       <p>Through Grey Remedy, Melissa explored the creation of art as a meditative practice while producing elegant, minimalist pieces that were carefully crafted with an emphasis on the importance of shape, texture and proportion. Her goal was to create objects that brought beauty to our daily routines, turning the parts of our day that we tend to rush through into calm, intentional moments.</p>
 
-      <p>Through her work, Melissa explored the tension between contrast and harmony, balancing curves and clean lines to produce beautifully unexpected shape combinations. She maintained a simple palette of black, white, and earth tones to challenge herself to innovate with form, experimenting with dimension and configuration to create distinctive pieces that could speak for themselves with little addition of color.</p> 
+      <p>In her work, Melissa explored the tension between contrast and harmony, balancing curves and clean lines to produce beautifully unexpected shape combinations. She maintained a simple palette of black, white, and earth tones to challenge herself to innovate with form, experimenting with dimension and configuration to create distinctive pieces that could speak for themselves with little addition of color.</p> 
       </div>
 
       <div className= "aboutBlock">
